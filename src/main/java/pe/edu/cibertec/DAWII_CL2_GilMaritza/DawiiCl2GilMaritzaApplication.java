21c3aa00d380868b196983df33e6b0a4cfeadfe7
AnaGil27 <gilyalta27@gmail.com>
2023-10-28 19:58:42 -0500

    Primer commit

---
+++ src/main/java/pe/edu/cibertec/DAWII_CL2_GilMaritza/DawiiCl2GilMaritzaApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWII_CL2_GilMaritza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiCl2GilMaritzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiCl2GilMaritzaApplication.class, args);
+	}
+
+}
